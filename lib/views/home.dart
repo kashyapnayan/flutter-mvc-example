5dbef8495c5c10c20d472ff3ad02ee07cc5923e2
lavi <lavi.bhumca2015@gmail.com>
2020-09-25 17:42:18 +0530

    added theme to the project

---
+++ lib/views/home.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('MVC Demo'),
+      ),
+      body: Center(
+        child: Text('Hello World'),
+      ),
+    );
+  }
+}
